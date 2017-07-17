fa462fa5fb42fdd6ec43041650c786c45f014e76
wangqi <489280591@qq.com>
2017-07-17 23:10:00 +0800

    no message

---
+++ Assets/Script/GameMian.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TinyTeam.UI;
+
+public class GameMian : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        TTUIRoot root = TTUIRoot.Instance;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
