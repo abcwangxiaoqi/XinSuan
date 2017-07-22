d275f0a5f82c3efde338936977f049b50f1a0dc7
wangqi <wangqi@pxsj.com>
2017-07-22 17:25:41 +0800

    no message

---
+++ Assets/Script/Moudle/ConfigMoudle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigMoudle
+{
+    public string ID;
+    public string MathTemplate;
+    public string NumsTemplate;
+    public string Count;
+    public string everyScore;
+}
