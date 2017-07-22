27af1ba0788c7d7416fb40aca31dbb10995a4ef8
wangqi <wangqi@pxsj.com>
2017-07-22 18:00:59 +0800

    no message

--- Assets/Script/Moudle/ConfigMoudle.cs
+++ Assets/Script/Moudle/ConfigMoudle.cs
@@ -2,11 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConfigMoudle
+public class ConfigJsonMoudle
 {
     public string ID;
     public string MathTemplate;
     public string NumsTemplate;
     public string Count;
     public string everyScore;
+
+    public ConfigMoudle Transfer()
+    {
+        ConfigMoudle moudle = new ConfigMoudle();
+        moudle.Count = int.Parse(this.Count);
+        moudle.everyScore = float.Parse(this.everyScore);
+
+        string[] maths = MathTemplate.Split(',');
+        moudle.MathTemplate = new List<string>(maths);
+
+        string[] nums = NumsTemplate.Split(',');
+        List<float> fnums = new List<float>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            fnums.Add(float.Parse(nums[i]));
+        }
+
+        moudle.NumsTemplate = fnums;
+
+        return moudle;
+    }
+}
+
+public class ConfigMoudle
+{
+    public int ID;
+    public List<string> MathTemplate;
+    public List<float> NumsTemplate;
+    public int Count;
+    public float everyScore;
+
+
+    int curIndex = 0;
+
+    public bool next(out string msg)
+    {
+        curIndex += 1;
+
+        msg = null;
+        return true;
+    }
 }
